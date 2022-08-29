639cecf1afc4694796ba6509dcb018f01693e3e2
britneywwc <britneywwc@gmail.com>
2022-08-29 16:46:18 +1000

    Creating custom components and passing props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.76,
+      date: new Date(2021, 3, 10),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: 1200.59,
+      date: new Date(2021, 4, 14),
+    },
+    {
+      id: "e4",
+      title: "Gaming Setup",
+      amount: 5325.28,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(props) {
+  // Passed props is just a Date object (for this example)
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
